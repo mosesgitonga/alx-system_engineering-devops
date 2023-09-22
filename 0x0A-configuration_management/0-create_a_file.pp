e0a9af3373547a10940c5bd641cf83a3d7d0c10f
mosesgitonga <infosec947@gmail.com>
2023-09-22 12:54:22 +0300

    replaced an - in owner and group

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 #creating a file
-file { '/tmp/school':
+file {'/tmp/school':
         ensure  => file,
         mode    => '0744',
-        owner   => 'www.data',
-        group   => 'www.data',
+        owner   => 'www-data',
+        group   => 'www-data',
         content => 'I love Puppet',
 }
