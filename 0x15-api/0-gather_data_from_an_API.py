9a270cc3d715b64c2d00bb764535208ea933085e
mosesgitonga <infosec947@gmail.com>
2023-10-23 15:07:57 +0300

    script for requests incomplete

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+a Python script that, using this REST API, for a given 
+employee ID, returns information about his/her TO DO list progress.
+"""
+import requests
+response = requests.get('https://jsonplaceholder.typicode.com/todos/1')
+print(response)
