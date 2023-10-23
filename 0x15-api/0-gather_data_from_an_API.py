ba9ab6af9484a90438b1eab97f0d3040351d6bb9
mosesgitonga <infosec947@gmailc.om>
2023-10-23 05:48:41 -0700

    emloyess tasks api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,5 +4,27 @@ a Python script that, using this REST API, for a given
 employee ID, returns information about his/her TO DO list progress.
 """
 import requests
-response = requests.get('https://jsonplaceholder.typicode.com/todos/1')
-print(response)
+import sys
+
+employeeID = sys.argv[1]
+
+base_url = 'https://jsonplaceholder.typicode.com/users'
+url = base_url + '/' + employeeID
+response = requests.get(url)
+employeeName = response.json().get('name')
+
+todo_url = url + '/todos'
+response = requests.get(todo_url)
+tasks = response.json()
+
+done_tasks = []
+num_of_done_tasks = 0
+
+for task in tasks:
+    if task.get('completed'):
+        done_tasks.append(task)
+        num_of_done_tasks += 1
+print('Employee {} is done with tasks({}/{}):'.format(employeeName, num_of_done_tasks,len(tasks)))
+
+for task in done_tasks:
+    print('\t {}'.format(task.get('title')))
