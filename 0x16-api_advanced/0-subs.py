edba3150434a72e038663ce2658826cb10cd7935
mosesgitonga <infosec947@gmail.com>
2023-11-07 14:18:43 +0300

    find num of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,27 @@
+
+  
+#!/usr/bin/python3
+"""
+number of subscribers for a given subreddit
+"""
+import requests
+from requests import get
+import sys
+
+def number_of_subscribers(subreddit):
+    """
+    find num of suscribers
+    """
+    if subreddit is None or not isinstance(subreddit, str):
+        print('s')
+        return 0
+
+    user_agent = {"user-agent": "O1UydnAcwk03Z2sAuoef8g"}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+
+    response = requests.get(url, headers=user_agent)
+    res = response.json()
+    try:
+        return res.get('data').get('subscribers')
+    except Exception:
+        return 0
